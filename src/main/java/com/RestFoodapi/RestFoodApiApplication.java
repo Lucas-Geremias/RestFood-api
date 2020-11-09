8adc5924f7c50d4fa01cdf36e1a5a6bb3e2a4c7a
Lucas-Geremias <geremiaslucas2012@gmail.com>
2020-11-09 09:46:29 -0300

    :package: começando o prejeto

---
+++ src/main/java/com/RestFoodapi/RestFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.RestFoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RestFoodapi/RestFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestFoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
